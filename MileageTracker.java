f51866f49b9b8158d104a9490ab8a625969cd8c0
Geoff Schmit <gcschmit@gmail.com>
2022-09-14 14:23:40 -0500

    define instance variables and constructors

--- MileageTracker.java
+++ MileageTracker.java
@@ -8,17 +8,55 @@ public class MileageTracker
 {
     /*
      * 2. Define the instance variables (store the object's attributes):
+     *      specify the visibility (e.g., private)
+     *          public: accessible by any code in any class
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., int, double, String)
+     *      specify the name (e.g., distanceDriven)
+     *      
+     *  Instance variables differ from local variables in the following ways:
+     *      scoped to the class (accessible in all methods of the class;
+     *          lifetime is the same as the object)
+     *      automatically initialized to a default value (0, false, null)
+     *      best practice is not to immediately initialize instance variables
      */
-
-
-
-
-
-
+    private int distanceDriven;     // in units of miles
+    private int fuelConsumed;       // in units of gallons
+    private String vin;             // vehicle identification
+    
     
     /*
      * 3. Define the constructor(s):
+     *      responsible for initializing newly created objects
+     *      invoked automatically via the new operator
+     *      has no return type (not even void)
+     *      name of the constructor must match the name of the class
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another contructor (with restrictions)
      */
+    
+    /**
+     * Default constructor for the MileageTracker class.
+     *      Initializes the object's miles driven and fuel consumed to 0
+     *          and the VIN to null.
+     */
+    public MileageTracker()
+    {
+        
+    }
+    
+    /**
+     * Constructs a new MileageTracker object with the specified
+     *      miles driven and fuel consumed.
+     *      
+     *  @param initialDistanceDriven the number of miles already driven
+     *  @param initialFuelConsumed the number of gallons of fueld already consumed
+     */
+    public MileageTracker(int initialDistanceDriven, int initialFuelConsumed)
+    {
+        
+    }
+    
     
     
     
@@ -54,10 +92,6 @@ public class MileageTracker
     {
         return 0;
     }
-    
-    
-    
-    
     
     /**
      * Increment the number of gallons of fuel that this car has consumed

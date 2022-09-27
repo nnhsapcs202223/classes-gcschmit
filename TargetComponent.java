dbe5028cd8900bc3d63ac5b4a39b3b5af460d132
Geoff Schmit <gcschmit@gmail.com>
2022-09-27 15:51:26 -0500

    added incremental steps

--- TargetComponent.java
+++ TargetComponent.java
@@ -12,13 +12,25 @@ public class TargetComponent extends JComponent
     public void paintComponent(Graphics g)
     {  
         Graphics2D g2 = (Graphics2D) g;  // cast to Graphics2D object
+        
+        // Step 1: compile and run the main method in TargetViewer;
+        //          verify that a window appears
+        
+        // Step 2: add code to create a new Ellipse2D.Double object
+        //          and draw it using the g2 variable;
+        //          verify that an ellipse appears
+        
+        
+        // Step 3: remove code from step 2; implement the Target class;
+        //          uncomment the following code;
+        //          verify that your target appears
 
         // create targets
-        Target target1 = new Target(0, 0);
-        Target target2 = new Target(150, 200);
+        //Target target1 = new Target(100, 150);
+        //Target target2 = new Target(200, 300);
         
         // draw targets
-        target1.draw(g2);
-        target2.draw(g2);      
+        //target1.draw(g2);
+        //target2.draw(g2);      
     }
 }
